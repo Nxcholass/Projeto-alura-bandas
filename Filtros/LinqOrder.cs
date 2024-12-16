c32e088748c89cf7a36a79a64d7c017ee8639858
Nicholas <ninovetti@gmail.com>
2024-12-16 13:30:26 -0300

    Primeiro Commit

---
+++ Filtros/LinqOrder.cs
@@ -0,0 +1,16 @@
+﻿using ScreenSound_04.Modelos;
+
+namespace ScreenSound_04.Filtros;
+
+internal class LinqOrder
+{
+    public static void ExibirListaDeArtistasOrdenados(List<Musica> musicas)
+    {
+        var artistarOrdenados = musicas.OrderBy(musica => musica.Artista).Select(musica => musica.Artista).Distinct().ToList();
+        Console.WriteLine("Lista de artistas odenados");
+        foreach (var  artista in artistarOrdenados)
+        {
+            Console.WriteLine($"- {artista}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using ScreenSound_04.Modelos;
+using System.Text.Json;
+using ScreenSound_04.Filtros;
+
+using (HttpClient client = new HttpClient())
+{
+    try
+    {
+        string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+        //LinqFilter.FiltrarTodosOsGenerosMusicais(musicas);
+        //LinqOrder.ExibirListaDeArtistasOrdenados(musicas);
+        //LinqFilter.FiltrarArtistasPorGeneroMusical(musicas, "pop");
+        //LinqFilter.FiltrarMusicasDeUmArtista(musicas, "Travis Scott");
+        LinqFilter.ExibirMusicasComTonalidade(musicas, "C#");
+        
+
+
+        //var musicasPreferidasDoNicholas = new MusicasPreferidas("Nicholas");
+        //musicasPreferidasDoNicholas.AdicionarMusicasFavoritas(musicas[1]);
+        //musicasPreferidasDoNicholas.AdicionarMusicasFavoritas(musicas[989]);
+        //musicasPreferidasDoNicholas.AdicionarMusicasFavoritas(musicas[1234]);
+        //musicasPreferidasDoNicholas.AdicionarMusicasFavoritas(musicas[456]);
+        //musicasPreferidasDoNicholas.AdicionarMusicasFavoritas(musicas[4]);
+
+
+        //var musicasPreferidasDaMirella = new MusicasPreferidas("Mirella");
+
+        //musicasPreferidasDaMirella.AdicionarMusicasFavoritas(musicas[5]);
+        //musicasPreferidasDaMirella.AdicionarMusicasFavoritas(musicas[1954]);
+        //musicasPreferidasDaMirella.AdicionarMusicasFavoritas(musicas[1000]);
+        //musicasPreferidasDaMirella.AdicionarMusicasFavoritas(musicas[95]);
+        //musicasPreferidasDaMirella.AdicionarMusicasFavoritas(musicas[800]);
+
+        //musicasPreferidasDaMirella.ExibirMusicasFavoritas();
+
+        //musicasPreferidasDaMirella.GerarArquivoJson();
+
+
+
+
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Temos um problema: {ex.Message}");
+    }
+}
